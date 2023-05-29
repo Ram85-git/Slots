673c269a7f9dccfc49cf7e774bff83b86d9867d4
Ram <126403725+Ram85-git@users.noreply.github.com>
2023-05-29 12:40:47 -0400

    first commit

---
+++ src/components/Home.vue
@@ -0,0 +1,49 @@
+<template>
+<Child>
+<template v-slot:header>
+    <img class="img1" src="https://www.onlc.com/blog/wp-content/uploads/2017/07/ONLC-2017-4.png"/>
+</template>
+
+<template v-slot:title>
+  <h3>Programming</h3>
+</template>
+
+<template v-slot:main>
+<p>Programming is the process of creating a set of instructions that tell a computer how to perform a task. </p>
+
+</template>
+
+<template v-slot:footer>
+<button>Click me</button>
+</template>
+
+</Child>
+</template>
+
+<script>
+import Child from './Child.vue';
+export default {
+    name: `Home`,
+    components: {
+        Child
+
+    }
+
+
+}
+
+
+
+</script>
+<style scoped>
+h1 {
+    color: orange;
+}
+.img1{
+    height: 300px;
+    width: 396px;
+
+
+}
+
+</style>
